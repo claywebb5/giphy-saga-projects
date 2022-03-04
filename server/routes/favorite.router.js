584d5b5537e3112b68e8948a0e47c7dae3ad3b4b
Clay Webb <clay_webb@outlook.com>
2022-03-04 05:25:26 -0600

    favorite.router complete

--- server/routes/favorite.router.js
+++ server/routes/favorite.router.js
@@ -6,13 +6,13 @@ const router = express.Router();
 // return all favorite images
 router.get('/', (req, res) => {
   // let queryText = 'SELECT * FROM favorites;';
-  let queryText = 'SELECT favorites.id, favorites.url, favorites.category_id, category.name FROM favorites JOIN category ON favorites.category_id = category.id;';
+  let queryText = 'SELECT favorites.id, favorites.url, favorites.category_id, category.name FROM favorites JOIN category ON favorites.category_id = category.id ORDER BY "id" ASC;'
   pool.query(queryText)
-  .then((result) => {
-    // console.log('Success!');
+  .then(result => {
+    console.log('Success!');
     res.send(result.rows);
-  }).catch((err) => {
-    console.log('Error in get on favorite.router.js!!', err);
+  }).catch((error) => {
+    console.log('Error in get on favorite.router.js!!', error);
     res.sendStatus(500);
   })
 });
@@ -21,15 +21,16 @@ router.get('/', (req, res) => {
 router.post('/', (req, res) => {
   let newFav = req.body;
   console.log('In favorite.router.js and the req.body is:', newFav);
-  let sqlQuery = `INSERT INTO "favorites" ("url", "name")
-                    VALUES ($1, $2);`;
-  // pool.query(queryText, [newFavorite.url, newFavorite.category_id])
-  pool.query(sqlQuery, [newFav.url, newFav.name])
 
-  .then((response) => {
-    console.log('Successful Post!!', response);
+  let queryText = `INSERT INTO "favorites" ("url", "name")
+                    VALUES ($1, $2);`;
+  pool.query(queryText, [newFav.url, newFav.category_id])
+  // pool.query(, [newFav.url, newFav.name])
+
+  .then(result => {
+    console.log('Successful Post!!');
     
-    res.sendStatus(200);
+    res.sendStatus(201);
   }).catch((error) => {
     console.log(error);
     res.sendStatus(500);
@@ -39,13 +40,29 @@ router.post('/', (req, res) => {
 
 // update given favorite with a category id
 router.put('/:favId', (req, res) => {
-  // req.body should contain a category_id to add to this favorite image
-  res.sendStatus(200);
+  console.log('logging body', req.body.category_id );
+  let category_id = req.body.category_id;
+
+  let queryString = `UPDATE "favorites" SET category_id = ${category_id} WHERE "id" = ${req.params.favId}`
+  pool.query(queryString).then((results) => {
+    res.sendStatus(200);
+  }).catch((err) => {
+    res.sendStatus(500);
+    console.log(err);
+  })
 });
 
 // delete a favorite
-router.delete('/', (req, res) => {
-  res.sendStatus(200);
+router.delete('/:id', (req, res) => {
+
+  let gifId = req.body.gifId;
+  let queryText = `DELETE FROM favorites WHERE id=$1`
+  pool.query(queryText, [gifId]).then((results) => {
+    res.sendStatus(200);
+  }).catch((err) => {
+    res.sendStatus(500);
+    console.log(err);
+  })
 });
 
 module.exports = router;
